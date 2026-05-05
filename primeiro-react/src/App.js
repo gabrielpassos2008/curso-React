1486b0f27f7943bdf59b7a32a9c28e4b9dd94993
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-05-05 20:19:41 -0300

    - aprendendo a mexer no css junto com codigo JS no app.

--- primeiro-react/src/App.js
+++ primeiro-react/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "gabriel"
   return (
     <div className="App">
-      <h1>primeiro projeto </h1>
-      <p>terminei meu curso semana passada!!!!</p>
+      <h1 className='titulo'>primeiro projeto </h1>
+      <p className='tituloNome'>ola {name}</p>
+      <p className='text'>terminei meu curso semana passada!!!!</p>
     </div>
   );
 }
