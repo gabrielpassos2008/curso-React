c6743b8a799e75caf734fa96beba8ea2788b5c13
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-05-07 21:44:04 -0300

    usando o defoult agora, caso o valor nao seja enviado.

--- primeiro-react/src/components/Item.js
+++ primeiro-react/src/components/Item.js
@@ -1,17 +1,22 @@
-import propTypes from "prop-types"
+import PropTypes from "prop-types"
 import styles from './Item.module.css'
-function Item({marca, ano}){
-    return(
-        <>
-            <li className={styles.item}>
-                {marca} - {ano}
-            </li>
-        </>
-    )
 
-Item.prototype= {
-    marca: propTypes.string.isRequired,
-    ano: propTypes.number.isRequired
+function Item({ marca, ano }) {
+    return (
+        <li className={styles.item}>
+            {marca} - {ano}
+        </li>
+    )
 }
+
+Item.propTypes = {
+    marca: PropTypes.string,
+    ano: PropTypes.number
 }
+
+Item.defaultProps = {
+    marca: "faltou a marca",
+    ano: 0
+}
+
 export default Item
