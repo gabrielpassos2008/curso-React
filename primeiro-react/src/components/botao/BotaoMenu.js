12b72f4c6a81e94e2d3a3fe29aad37e675b18a35
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-05-10 19:15:12 -0300

    - adicionei o botao da navbar. - adicionei o module de css para esse botao.

---
+++ primeiro-react/src/components/botao/BotaoMenu.js
@@ -0,0 +1,10 @@
+import styles from "./botaoMenu.module.css"
+
+function BotaoMenu({conteudo}){
+    return(
+        <>
+            <a> {conteudo}</a>
+        </>
+    );
+}
+export default BotaoMenu
