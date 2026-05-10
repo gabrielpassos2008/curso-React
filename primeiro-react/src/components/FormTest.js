16f143aaa528c1df76a9bb715366da83471dbd1a
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-05-10 19:44:21 -0300

    - criei um novo form para aprender o useState

---
+++ primeiro-react/src/components/FormTest.js
@@ -0,0 +1,17 @@
+function FromTest(){
+    return(
+        <form>
+            <h1>login</h1>
+
+            <label>email:</label>
+            <input type="text" name="email"></input>
+
+            <label>senha:</label>
+            <input type="text" name="senha"></input>               
+
+            <button>logar</button>
+        </form>
+    );
+}
+
+export default FromTest
--- primeiro-react/src/components/form/FormLogin.js
+++ primeiro-react/src/components/form/FormLogin.js
@@ -1,13 +1,20 @@
 import Botao from "../botao/Botao"
 import Input from "../input/Input";
 import styles from "./formLogin.module.css"
+import {useState} from "react"
 function FormLogin() {
+
+    const [nome, setNome] = useState()
+    const [senha, setSenha] = useState()
+
+    console.log(senha)
+    console.log(nome)
     return (
         <>
             <form>
                 <h2>Faça seu login:</h2>
                 <label>Email:</label>
-                <Input tipo="text" nome="email"></Input>
+                <Input tipo="text" nome="email" ></Input>
 
                 <label>Senha:</label>
                 <Input tipo="text" nome="senha"></Input>
