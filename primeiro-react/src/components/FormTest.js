dab0a72f3c56c51422f1a2c5a943dbdae1718574
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-05-10 19:55:06 -0300

    - adicionei o primeiro useState para coletar os dados.

--- primeiro-react/src/components/FormTest.js
+++ primeiro-react/src/components/FormTest.js
@@ -1,13 +1,24 @@
+import {useState} from "react"
+
 function FromTest(){
+    const [nome, setNome] = useState()
+    const [senha, setSenha] = useState()
+
+    function cadastrarUsuario(evento){
+        evento.preventDefault()
+        console.log(senha)
+        console.log(nome)
+    }
+
     return(
-        <form>
+        <form onSubmit={cadastrarUsuario}>
             <h1>login</h1>
 
             <label>email:</label>
-            <input type="text" name="email"></input>
+            <input type="text" name="email" onChange={(evento) => setNome(evento.target.value)}></input>
 
             <label>senha:</label>
-            <input type="text" name="senha"></input>               
+            <input type="text" name="senha" onChange={(evento) => setSenha(evento.target.value)}></input>               
 
             <button>logar</button>
         </form>
